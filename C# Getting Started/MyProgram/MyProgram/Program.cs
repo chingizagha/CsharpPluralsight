f9afb05a37d418ff658fa7b79bf17c712418adbc
chingizagha <cingiz167@gmail.com>
2022-02-04 17:43:06 +0400

    C# Program flow

---
+++ C# Getting Started/MyProgram/MyProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MyUtilities;
+
+namespace MyProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WeatherUtilities.Report("Sumgait", 23, 65);
+
+
+
+        }
+    }
+}
---
+++ Controlling Program Flow in C#/02/demos/start/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void GetPercentage(double surveyed, double responded)
+            {
+                var respondedPercentage = (100 * responded) / surveyed;
+                var notResponded = surveyed - responded;
+
+                double overallScore = (Q1Results.ServiceScore + Q1Results.PriceScore + Q1Results.FoodScore + Q1Results.CoffeeScore) / 4;
+
+                Console.WriteLine($"Percentage of responders: {respondedPercentage:N1}%");
+                Console.WriteLine($"Number of unanswered: {notResponded}");
+                Console.WriteLine($"Overall score of caffee: {overallScore}");
+            }
+
+            var list = new List<string>();
+
+            GetPercentage(Q1Results.NumberSurveyed, Q1Results.NumberResponded);
+        }
+    }
+}
