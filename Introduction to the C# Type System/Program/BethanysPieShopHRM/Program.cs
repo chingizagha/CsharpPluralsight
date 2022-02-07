a5a260b055251669ab84171334e6287fd68f60f3
chingizagha <cingiz167@gmail.com>
2022-02-07 12:25:23 +0400

    Update

---
+++ Introduction to the C# Type System/Program/BethanysPieShopHRM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace BethanysPieShopHRM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var num = Calculate(a: 5, b: 6);
+            Console.WriteLine(num);
+            static int Calculate(int a, int b) => a + b;
+        }
+    }
+}
