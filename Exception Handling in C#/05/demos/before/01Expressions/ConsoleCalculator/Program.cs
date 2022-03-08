819685ffda1c7edb5ca1dcf1d30fe8e864de5828
chingizagha <cingiz167@gmail.com>
2022-03-08 11:50:58 +0400

    Update

---
+++ Exception Handling in C#/05/demos/before/01Expressions/ConsoleCalculator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculator();
+
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Sorry, something went wrong. {ex}");
+            }
+            
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+    }
+}
+
+
+// FROM C# 9 "Top-level statements":
+
+//using ConsoleCalculator;
+//using static System.Console;
+
+
+//WriteLine("Enter first number");
+//int number1 = int.Parse(ReadLine());
+
+//WriteLine("Enter second number");
+//int number2 = int.Parse(ReadLine());
+
+//WriteLine("Enter operation");
+//string operation = ReadLine().ToUpperInvariant();
+
+
+//var calculator = new Calculator();
+//int result = calculator.Calculate(number1, number2, operation);
+//DisplayResult(result);
+
+
+//WriteLine("\nPress enter to exit.");
+//ReadLine();
+
+
+//void DisplayResult(int result)
+//{
+//    WriteLine($"Result is: {result}");
+//}
+
+
---
+++ Exception Handling in C#/05/demos/before/04Rethrowing/ConsoleCalculator/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Enter first number");
+            int number1 = int.Parse(ReadLine());
+
+            WriteLine("Enter second number");
+            int number2 = int.Parse(ReadLine());
+
+            WriteLine("Enter operation");
+            string operation = ReadLine().ToUpperInvariant();
+
+
+            var calculator = new Calculator();
+
+            try
+            {
+                int result = calculator.Calculate(number1, number2, operation);
+                DisplayResult(result);
+            }
+            catch (ArgumentNullException ex)
+            {
+                // Log.Error(ex);
+                WriteLine($"Operation was not provided. {ex}");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                // Log.Error(ex);
+                WriteLine($"Operation is not supported. {ex}");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Sorry, something went wrong. {ex}");
+            }
+            finally
+            {
+                WriteLine("...finally...");
+            }
+            
+
+
+            WriteLine("\nPress enter to exit.");
+            ReadLine();
+        }
+
+        private static void DisplayResult(int result) => WriteLine($"Result is: {result}");
+    }
+}
+
+
+// FROM C# 9 "Top-level statements":
+
+//using ConsoleCalculator;
+//using static System.Console;
+
+
+//WriteLine("Enter first number");
+//int number1 = int.Parse(ReadLine());
+
+//WriteLine("Enter second number");
+//int number2 = int.Parse(ReadLine());
+
+//WriteLine("Enter operation");
+//string operation = ReadLine().ToUpperInvariant();
+
+
+//var calculator = new Calculator();
+//int result = calculator.Calculate(number1, number2, operation);
+//DisplayResult(result);
+
+
+//WriteLine("\nPress enter to exit.");
+//ReadLine();
+
+
+//void DisplayResult(int result)
+//{
+//    WriteLine($"Result is: {result}");
+//}
+
+
