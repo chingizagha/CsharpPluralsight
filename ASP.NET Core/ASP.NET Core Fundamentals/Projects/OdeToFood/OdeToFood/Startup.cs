1174d9f6d5cafe79f630e20e7778448234f51898
chingizagha <cingiz167@gmail.com>
2022-04-30 22:18:13 +0400

    Update

--- ASP.NET Core/ASP.NET Core Fundamentals/Projects/OdeToFood/OdeToFood/Startup.cs
+++ ASP.NET Core/ASP.NET Core Fundamentals/Projects/OdeToFood/OdeToFood/Startup.cs
@@ -46,21 +46,19 @@ namespace OdeToFood
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules();
 
+            // aspnetcore30
             app.UseRouting();
-            app.UseAuthorization();
-            
-
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(e =>
             {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
+                e.MapRazorPages();
+                e.MapControllers();
             });
         }
     }
